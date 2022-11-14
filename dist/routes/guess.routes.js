4d1a0911190720eba63737a8ea56e457e2227373
Francksuel Machado <107144900+Francksuel@users.noreply.github.com>
2022-11-14 10:47:56 -0300

    Delete dist directory

--- dist/routes/guess.routes.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { createGuess } from "../controllers/guess.controllers.js";
-var guessRouter = express.Router();
-guessRouter.post("/guess/:matchId", createGuess);
-export { guessRouter };
